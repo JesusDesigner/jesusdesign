9d6ac97ad680976be33eee64bbd352951b5812a1
jesusdesigner <jesuspunck@hotmail.com>
2023-03-04 16:55:10 -0300

    atyalização
    13

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -26,9 +26,9 @@ export default function Footer() {
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap text-center lg:text-left">
             <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl font-semibold">Let's keep in touch!</h4>
+              <h4 className="text-3xl font-semibold">Minhas Redes Sociais!</h4>
               <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-                Find us on any of these platforms, we respond 1-2 business days.
+              Sigam Minhas Redes SNociais
               </h5>
               <div className="mt-6 lg:mb-0 mb-6">
                 <button
@@ -61,7 +61,7 @@ export default function Footer() {
               <div className="flex flex-wrap items-top mb-6">
                 <div className="w-full lg:w-4/12 px-4 ml-auto">
                   <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
-                    Useful Links
+                    Links
                   </span>
                   <ul className="list-unstyled">
                     <li>
--- src/views/Index.js
+++ src/views/Index.js
@@ -250,14 +250,16 @@ export default function Index() {
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap justify-center text-center mb-24">
               <div className="w-full lg:w-6/12 px-4">
-                <h2 className="text-4xl font-semibold">Here are our heroes</h2>
+                <h2 className="text-4xl font-semibold">Portiflio</h2>
                 <p className="text-lg leading-relaxed m-4 text-blueGray-500">
                   According to the National Oceanic and Atmospheric
                   Administration, Ted, Scambos, NSIDClead scentist, puts the
                   potentially record maximum.
                 </p>
+                
               </div>
             </div>
+            
             <section className="block relative z-1 bg-blueGray-600">
         <div className="container mx-auto">
           <div className="justify-center flex flex-wrap">
