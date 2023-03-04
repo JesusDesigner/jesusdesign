7bd413826ac5ce3c2a9c29c81c708895bc87b70e
jesusdesigner <jesuspunck@hotmail.com>
2023-03-04 17:54:05 -0300

    footer
    footer

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -28,7 +28,7 @@ export default function Footer() {
             <div className="w-full lg:w-6/12 px-4">
               <h4 className="text-3xl font-semibold">Minhas Redes Sociais!</h4>
               <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
-              Sigam Minhas Redes SNociais
+              Sigam Minhas Redes Sociais
               </h5>
               <div className="mt-6 lg:mb-0 mb-6">
                 <button
